3e3a0f2ca629c52b60510876ca04e2a0b6a3a98d
Ahmed Salah Fathy Sotohy <laslsalsa123@gmail.com>
2026-08-27 20:39:04 +0300

    fix: correct ICreatePayment interface name and creat-payment directory typo

--- src/application/use-cases/create-payment/create-payment.use-case.ts
+++ src/application/use-cases/create-payment/create-payment.use-case.ts
@@ -1,4 +1,4 @@
-import { ICreatePayment } from "@application/use-cases/creat-payment/create-payment.input";
+import { CreatePaymentInput } from "@application/use-cases/create-payment/create-payment.input";
 import { PaymentRepository } from "@application/repositories/payment.repository";
 import { Money } from "@domain/value-objects/money.vo";
 import { Currency } from "@shared/constants/payment.constants";
@@ -8,7 +8,7 @@ export class CreatePaymentUseCase {
     constructor(
         private  paymentRepository: PaymentRepository,
     ) {}
-    async execute(input: ICreatePayment) {
+    async execute(input: CreatePaymentInput) {
         //1-check and convert (amount,currency) from Money
         const money = Money.from(input.amount, input.currency as Currency);
         //2 create Id for payment
